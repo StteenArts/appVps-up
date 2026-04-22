c8a85ae29a0ece846086d9ec640197c39635d0e7
stteenArts <stteenriwi@gmail.com>
2026-04-21 21:01:54 -0500

    add: function delete user

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -17,6 +17,7 @@ public class EventController : Controller
         var events = _context.events.ToList();
         return View(events);
     }
+    
     [HttpPost]
     public IActionResult Edit(int id, Events newEvents)
     {
@@ -28,6 +29,16 @@ public class EventController : Controller
         _context.SaveChanges();
         return RedirectToAction("Index");
     }
+    
+    [HttpPost]
+    public IActionResult Delete(int id)
+    {
+        var eventDelete = _context.events.Find(id);
+        _context.events.Remove(eventDelete);
+        _context.SaveChanges();
+        return RedirectToAction("Index");
+    }
+    
     [HttpPost]
     public IActionResult Store(Events events)
     {
--- Models/Events.cs
+++ Models/Events.cs
@@ -5,6 +5,6 @@ public class Events
     public int Id { get; set; }
     public string Name { get; set; }
     public DateTime Date { get; set; }
-    public string Description { get; set; }
-    public string Status { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
 }
